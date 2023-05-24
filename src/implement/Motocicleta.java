deea3ee5236b2dc216d4470b6b4ed149c4ed48e8
XandyGomes <x3.computadores@hotmail.com>
2023-05-23 22:13:23 -0300

    Aula.23.05.2023

---
+++ src/implement/Motocicleta.java
@@ -0,0 +1,15 @@
+package implement;
+
+public class Motocicleta extends Veiculo {
+  
+  @Override
+  public void acelerar(Integer valor){
+
+  }
+
+  @Override
+  public void frear(Integer valor){
+
+  }
+
+}
